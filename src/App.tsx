8ee25538ad46d3acff1bd59be09f628a1426c437
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-07-09 11:30:34 -0600

    Revert "Form.tsx, /src/index.css (x6)"
    This reverts commit b7eb3773f4718ad11761c401f2050150a8e2709d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import Form from "./components/Form.tsx";
-import PacientesList from "./components/PacientesList.tsx";
-
 function App() {
     return (
         <>
-            <div className="container mx-auto mt-20">
-                <h1 className="font-black font-fjalla text-5xl text-center md:w-2/3 md:mx-auto">Seguimiento de Pacientes {" "} <span className="text-indigo-700">Veterinaria</span></h1>
-
-                <div className="mt-12 md:flex">
-                    <Form/>
-                    <PacientesList/>
-                </div>
-            </div>
+            <h2 className="">Hola mundo</h2>
         </>
     )
 }
